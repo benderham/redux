24b808d657e16ff18f4a8bee08b1d3972652adfe
Ben Derham <bderham@longtail.com.au>
2016-10-19 14:32:14 +0800

    initial commit

---
+++ reducers.js
@@ -0,0 +1,51 @@
+import {VisibilityFilters} from './actions'
+
+const initialState = {
+  visibilityFilter: VisibilityFilters.SHOW_ALL,
+  todos: []
+}
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false
+        }
+      ]
+      break
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+          if (index === action.index) {
+            return Object.assign({}, todo, {
+              completed: !todo.completed
+            })
+          }
+          return todo
+        })
+      break
+    default:
+      return state
+  }
+}
+
+function todoApp(state = initialState, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return Object.assign({}, state, {
+        visibilityFilter: action.filter
+      })
+      break
+    case ADD_TODO:
+      break
+    case TOGGLE_TODO:
+      return Object.assign({}, state, {
+        todos: todos(state.todos, action)
+      })
+      break
+    default:
+      return state
+  }
+}
